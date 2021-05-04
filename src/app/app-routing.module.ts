965d3a824944c6e7f5ad53ea77338d3478e913e6
randriii <noyohe6275@sumwan.com>
2021-05-04 04:19:17 +0300

    add create route to route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { CreateVoitureComponent } from "./create-voiture/create-voiture.component";
 import { VoitureListComponent } from "./voiture-list/voiture-list.component";
 
 const routes: Routes = [
     {path: 'voitures', component: VoitureListComponent},
+    {path: 'create-voiture', component: CreateVoitureComponent},
     {path: '', redirectTo: '/voitures', pathMatch: 'full'}
 ];
 
