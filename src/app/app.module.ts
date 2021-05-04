5c08abae21bb5817a047e9c503e54ef3dfea9afc
randriii <noyohe6275@sumwan.com>
2021-05-04 04:16:59 +0300

    import FormModule from angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { VoitureListComponent } from './voiture-list/voiture-list.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CreateVoitureComponent } from './create-voiture/create-voiture.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateVoitureComponent } from './update-voiture/update-voiture.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoitureListComponent
+    VoitureListComponent,
+    CreateVoitureComponent,
+    UpdateVoitureComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
